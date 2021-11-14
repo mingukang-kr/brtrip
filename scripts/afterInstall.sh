79c9814543341cdc7f1ccb7ec017b386ed72d78b
mingukang <mgkang0317@gmail.com>
2021-11-14 21:06:08 +0900

    docker hub 추가

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+REPOSITORY=mingukangkor
 APP_NAME=brtrip
 
 echo "> Build new app image"
-docker build -t $APP_NAME .
+docker pull $REPOSITORY/$APP_NAME
 
 echo "> Deploy new app container"
-docker run -it -d --name app $APP_NAME -p 8080:8080 /bin/bash
+docker run -it -d --name app $REPOSITORY/$APP_NAME -p 8080:8080 /bin/bash
--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -12,8 +12,9 @@ mkdir -vp $REPOSITORY/build
 if [ -n "$(docker ps -q -f name=app)" ]; then
   echo "> stop app container"
   docker stop app
-  if [ -n "$(docker ps -aq -f status=exited -f name=app)" ]; then
-    echo "> remove app container"
-    docker rm app
-  fi
+fi
+
+if [ -n "$(docker ps -aq -f status=exited -f name=app)" ]; then
+  echo "> remove app container"
+  docker rm app
 fi
